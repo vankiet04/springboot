334f1477c0fc14de55f3f3c1b1c3cdbe81e0428f
kietng04 <124132816+kietng04@users.noreply.github.com>
2024-08-01 18:03:55 +0700

    advanced search

---
+++ src/main/java/net/enjoy/springboot/registrationlogin/entity/Category.java
@@ -0,0 +1,23 @@
+package net.enjoy.springboot.registrationlogin.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String categoryName;
+
+}
---
+++ src/main/java/net/enjoy/springboot/registrationlogin/entity/Size.java
@@ -0,0 +1,27 @@
+package net.enjoy.springboot.registrationlogin.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sizes")
+public class Size {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String sizeName;
+
+    @Column(nullable = false)
+    private String sizeDescription;
+}
