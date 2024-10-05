ddeb99d27fb1940bc847a6417dd97fbcab81d2f4
phamkiet17 <phamkiet210504@gmail.com>
2024-10-05 17:52:06 +0700

    tôi push nhánh test3

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, TextField, Button, Typography, Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Container maxWidth="sm">
+      <Box display="flex" flexDirection="column" alignItems="center" mt={5}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Thêm Sản Phẩm đ
+        </Typography>
+        <form>
+          <TextField
+            label="Tên sản phẩm"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Giá sản phẩm"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            type="number"
+            required
+          />
+          <TextField
+            label="Mô tả sản phẩm"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            multiline
+            rows={4}
+            required
+          />
+          <Button variant="contained" color="primary" type="submit" fullWidth>
+            Thêm sản phẩm
+          </Button>
+        </form>
+      </Box>
+    </Container>
   );
 }
 
