ae8c1aa072cb16b7065f351e6fbd5a77d8e64a82
HaolorL <dangminhhao365@gmail.com>
2024-10-31 18:50:15 +0700

    new

---
+++ src/main/frontend/src/views/base/customer/customerService.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8080/api/customers'
+
+class customerService {
+  getCustomer() {
+    return axios.get(`${API_URL}/getall`)
+  }
+
+  getCustomerAtPage(page) {
+    return axios.get(`${API_URL}/getcustomerPage?page=${page}`)
+  }
+
+}
+
+export default new customerService()
